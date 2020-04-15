dea1e536c72870db6d89833d2cf8d7930f0657c6
Jacykow <jacgulij@gmail.com>
2020-04-15 13:08:40 +0200

    init

---
+++ app/src/main/java/com/gulij/moonslice/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gulij.moonslice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
